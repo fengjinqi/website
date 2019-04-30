082013d0b796f69a4560677deb6dbad8a76d2a39
fengjinqi <tarena_feng@126.com>
2019-04-30 18:13:59 +0800

    修复配置文件

--- website/settings.py
+++ website/settings.py
@@ -199,11 +199,7 @@ MEDIA_ROOT = (
     os.path.join(BASE_DIR, 'upload')
 )
 conf = ConfigParser()
-#conf.read(os.path.dirname(os.path.dirname('config.ini')))
 conf.read('config.ini')
-# conf.add_section('email')
-# conf.set('email','password','Feng19961024yao.')
-# conf.write(open('config.ini','w'))
 
 DOMAIN = 'http://127.0.0.1:8000'
 EMAIL_HOST = "smtp.exmail.qq.com"
@@ -254,14 +250,14 @@ CELERYBEAT_SCHEDULE = {
     },
 }
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # 引擎
-SESSION_CACHE_ALIAS = 'default'  # 使用的缓存别名（默认内存缓存，也可以是memcache），此处别名依赖缓存的设置
-
-SESSION_COOKIE_NAME = "sessionid"  # Session的cookie保存在浏览器上时的key，即：sessionid＝随机字符串
-SESSION_COOKIE_PATH = "/"  # Session的cookie保存的路径
-SESSION_COOKIE_DOMAIN = None  # Session的cookie保存的域名
-SESSION_COOKIE_SECURE = False  # 是否Https传输cookie
-#SESSION_COOKIE_HTTPONLY = True  # 是否Session的cookie只支持http传输
-SESSION_COOKIE_AGE = 1800  # Session的cookie失效日期（2周）
-SESSION_EXPIRE_AT_BROWSER_CLOSE = False  # 是否关闭浏览器使得Session过期
-SESSION_SAVE_EVERY_REQUEST = True  # 是否每次请求都保存Session，默认修改之后才保存
+# SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # 引擎
+# SESSION_CACHE_ALIAS = 'default'  # 使用的缓存别名（默认内存缓存，也可以是memcache），此处别名依赖缓存的设置
+#
+# SESSION_COOKIE_NAME = "sessionid"  # Session的cookie保存在浏览器上时的key，即：sessionid＝随机字符串
+# SESSION_COOKIE_PATH = "/"  # Session的cookie保存的路径
+# SESSION_COOKIE_DOMAIN = None  # Session的cookie保存的域名
+# SESSION_COOKIE_SECURE = False  # 是否Https传输cookie
+# #SESSION_COOKIE_HTTPONLY = True  # 是否Session的cookie只支持http传输
+# SESSION_COOKIE_AGE = 1800  # Session的cookie失效日期（2周）
+# SESSION_EXPIRE_AT_BROWSER_CLOSE = False  # 是否关闭浏览器使得Session过期
+# SESSION_SAVE_EVERY_REQUEST = True  # 是否每次请求都保存Session，默认修改之后才保存
