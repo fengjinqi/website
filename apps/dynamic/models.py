6ee82139efe6b256637cd846dab69f6bd5a50b18
冯金琪 <fengjinqi@fengjinqideMacBook-Pro.local>
2020-06-30 09:43:05 +0800

    添加编辑器

--- apps/dynamic/models.py
+++ apps/dynamic/models.py
@@ -17,6 +17,13 @@ class dynamic(models.Model):
     location = models.CharField(max_length=50, verbose_name="位置")
     add_time = models.DateTimeField(auto_now_add=True)
 
+    def get_number(self):
+        n = self.dynamiccomment_set.all()
+        num = self.dynamiccomment_set.count()
+        for i in n:
+            num += i.dynamiccommentreply_set.count()
+        return num
+
     def __str__(self):
         return str(self.id)
 
@@ -58,10 +65,11 @@ class dynamicComment(models.Model):
 
 class dynamicCommentReply(models.Model):
     """评论回复"""
-    dynamicUser = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='当前用户',related_name='dynamicUser')
-    dynamicToUser = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='目标用户',related_name='dynamicToUser',default='')
+    dynamicUser = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='当前用户', related_name='dynamicUser')
+    dynamicToUser = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='目标用户', related_name='dynamicToUser',
+                                      default='')
     comments = models.TextField(verbose_name='回复内容')
-    url = models.CharField(max_length=60,blank=True,null=True,default='')
-    aomments_id = models.ForeignKey(dynamicComment,on_delete=models.CASCADE,verbose_name='回复id')
-    address = models.CharField(max_length=50, verbose_name='地址',blank=True,null=True)
+    url = models.CharField(max_length=60, blank=True, null=True, default='')
+    aomments_id = models.ForeignKey(dynamicComment, on_delete=models.CASCADE, verbose_name='回复id')
+    address = models.CharField(max_length=50, verbose_name='地址', blank=True, null=True)
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
