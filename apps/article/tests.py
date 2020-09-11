0f69566e0785b73ee62d72d4feb7751fca7809ab
冯金琪 <fengjinqi@fengjinqideMacBook-Pro.local>
2020-09-11 11:00:56 +0800

    修复每天数据库备份发送附件

--- apps/article/tests.py
+++ apps/article/tests.py
@@ -27,32 +27,5 @@ if __name__ == '__main__':
     #print(n)
 
 
-    # 文件路径
 
-    address = '/Users/fengjinqi/fsdownload/website_20200910_010000.sql'
-print(os.path.exists(address))
-timer = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-fileName = '/data/website_{}.sql'.format(timer)
-print(fileName)
-print(os.path.exists(fileName))
-# 如名字所示Multipart就是分多个部分
-    # msg = MIMEMultipart()
-    # msg["Subject"] = "网站数据库备份"
-    # msg["From"] = _user
-    # msg["To"] = "1218525402@qq.com"
-    #
-    # # ---这是文字部分---
-    # part = MIMEText("网站数据库备份")
-    #
-    # msg.attach(part)
-    #
-    # # ---这是附件部分---
-    # # 类型附件
-    # part = MIMEApplication(open(os.path.abspath('/Users/fengjinqi/fsdownload/website_20200910_010000.sql'), 'rb').read())
-    # part.add_header('Content-Disposition', 'attachment', filename="abc_backup.sql")
-    # msg.attach(part)
-    # s = smtplib.SMTP("smtp.exmail.qq.com", timeout=30)  # 连接smtp邮件服务器,端口默认是25
-    # s.login(_user, _pwd)  # 登陆服务器
-    # s.sendmail(_user, _to, msg.as_string())  # 发送邮件
-    # s.close();
 
