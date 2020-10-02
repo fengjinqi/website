6c035ad296f12d68c220e7c7892f014d54af6f85
Anirban Bhattacharya <70073894+AnirbanB999@users.noreply.github.com>
2020-10-02 12:55:33 +0530

    Update and rename manage.py to hkmanage.py

--- hkmanage.py
+++ hkmanage.py
@@ -13,3 +13,5 @@ if __name__ == "__main__":
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+    #This is for hacktoberfest. Please dont delete
